08084a7557659008cf7250aee6107473e5946208
Nuria <nuria.villaronga@xercode.com>
2022-04-20 17:51:00 +0200

    funcion editar completa --> falta eliminar + añadir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,171 @@
-import logo from './logo.svg';
+import React, { useState, Fragment } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
+import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import userData from "./users-data.json"
+
+//"xs","lg","sm","1x","2x","3x","4x","5x","6x","7x","8x","9x","10x"; 
+const IconAction = ({ id, size = '2x' }) => {
+
+  let imgIcon = '';
+  if(id == "delete") {
+      imgIcon = faTrashCan;
+  }
+  else if(id == "save") {
+      imgIcon = faFloppyDisk;
+  }
+  else if(id == "update") {
+      imgIcon = faPenToSquare;
+  }
+  else if(id == "cancel") {
+      imgIcon = faXmark;
+  }
+
+  return (<div id={id} className="iconBtn">
+    <FontAwesomeIcon icon={imgIcon} size={size} className="icons"/>
+  </div>);   
+}; 
+
+const ReadOnlyRow = ({ element, handleEdit }) => {
+  return (<tr>
+    <td>{element.name}</td>
+    <td>{element.email}</td>
+    <td>{element.rol}</td>
+    <td>
+        <button type="button" onClick={(event) => handleEdit(event, element)}><IconAction id="update" size="2x"/></button>
+        <button><IconAction id="delete" size="2x"/></button>
+    </td>
+  </tr>); 
+}
+
+const EditableRow = ({ editForm, handleEditChange, handleCancel }) => {
+  return (<tr>
+    <td><input className="form-control" type="text" required="required" name="name" value={editForm.name} onChange={handleEditChange}></input></td>
+    <td><input className="form-control" type="text" required="required" name="email" value={editForm.email} onChange={handleEditChange}></input></td>
+    <td> 
+      <select className="form-select" required="required" name="rol" value={editForm.rol} onChange={handleEditChange}>
+        <option>Admin</option>
+        <option>User</option>
+      </select>
+    </td>
+    <td>
+        <button type="submit"><IconAction id="save" size="2x"/></button>
+        <button type="button" onClick={handleCancel}><IconAction id="cancel" size="2x"/></button>
+    </td>
+  </tr>); 
+}
+
+const TableCrud = ({ filter }) => {
+
+  const [users, setUsers] = useState(userData);
+
+  const [editUserId, setEditUserId] = useState(null);
+
+  const [editFormData, setEditFormData] = useState({
+    name : "",
+    email: "",
+    rol: "",
+  })
+
+  const handleEditClick = (event, user) => {
+    event.preventDefault(); //preventDefault() cancela el evento si es cancelable, lo que significa que la acción predeterminada que pertenece al evento no ocurrirá.
+    setEditUserId(user.id);
+
+    const formValues = {
+      name : user.name,
+      email: user.email,
+      rol: user.rol,
+    }
+
+    setEditFormData(formValues);
+  }
+
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData  };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+
+  }
+
+  const handleSaveFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedUser = {
+      id : editUserId,
+      name : editFormData.name,
+      email: editFormData.email,
+      rol: editFormData.rol,
+    }
+
+    const newUsers = [...users];
+    const index = users.findIndex((user) => user.id === editUserId);
+
+
+    newUsers[index] = editedUser;
+    setUsers(newUsers);
+    setEditUserId(null);
+  }
+
+  const handleCancelClick = () => {
+    setEditUserId(null);
+  }
+
+  //---------Falta--------------
+  const handleDeleteClick = () => {
+
+  }
+
+  let attrFilter = '';
+  if(filter == false) {
+    attrFilter = 'hidde';
+  }
+  
+  return (<form onSubmit={handleSaveFormSubmit}>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className={attrFilter}> 
+          <td><input className="form-control"></input></td>
+          <td><input className="form-control"></input></td>
+          <td><input className="form-control"></input></td>
+          <td></td>
+        </tr>
+        {
+          users.map((user) => (
+            <Fragment key={user.id}>
+              {
+                editUserId === user.id ? <EditableRow editForm={editFormData} handleCancel={handleCancelClick} handleEditChange={handleEditFormChange}/> : <ReadOnlyRow element={user} handleEdit={handleEditClick}/>
+              }
+            </Fragment>
+          ))
+        }
+      </tbody>
+    </table>
+    </form>);   
+}; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <TableCrud filter={false}/>
     </div>
   );
 }
