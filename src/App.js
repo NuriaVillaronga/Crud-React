776a1c613b4ed5c45e12a7fe0cd013dba3f1fa37
Nuria <nuria.villaronga@xercode.com>
2022-04-21 16:31:22 +0200

    start with subtable + added form new User

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, Fragment } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import {Table, Button, Container, Modal, ModalBody, ModalHeader, FormGroup, ModalFooter} from 'reactstrap';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import userData from "./users-data.json"
 
 //"xs","lg","sm","1x","2x","3x","4x","5x","6x","7x","8x","9x","10x"; 
@@ -27,6 +28,14 @@ const IconAction = ({ id, size = '2x' }) => {
       imgIcon = faPenToSquare;
       iconColor = "rgb(63, 109, 69)";
   }
+  else if(id == "arrowDown") {
+    imgIcon = faArrowDown;
+    iconColor = "grey";
+  }
+  else if(id == "arrowUp") {
+    imgIcon = faArrowUp;
+    iconColor = "grey";
+  }
   else if(id == "cancel") {
       imgIcon = faXmark;
       iconColor = "rgb(185, 9, 9)"; 
@@ -36,16 +45,36 @@ const IconAction = ({ id, size = '2x' }) => {
 }; 
 
 
-const ReadOnlyRow = ({ element, handleEdit, handleDelete }) => {
-  return (<tr>
-    <td>{element.name}</td>
+const ReadOnlyRow = ({ element, handleEdit, handleDelete, handleDisplay }) => {
+  return (<Fragment>
+    <tr>
+    <td><button className="btnIcon" onClick={() => handleDisplay(element.id)}><IconAction id="arrowUp" size="1x"/></button>{element.name}</td>
     <td>{element.email}</td>
     <td>{element.rol}</td> 
     <td>
         <button className="btnIcon" type="button" onClick={(event) => handleEdit(event, element)}><IconAction id="update" size="2x"/></button>
         <button className="btnIcon" type="button" onClick={() => handleDelete(element.id)}><IconAction id="delete" size="2x"/></button>
     </td>
-  </tr>); 
+  </tr>
+  <tr className='row-suplementary-table' id={`row-suplementary${element.id}`}>
+      <td colspan="4">
+        <div className='div-suplementary-table'>
+          <table className='suplementary-table'>
+            <tr>
+              <th>Direccion</th>
+              <th>CP</th>
+              <th>Teléfono</th>
+            </tr>
+            <tr>
+              <td>dfagdfgdh</td>
+              <td>fddfhdfh</td>
+              <td>dfhdfhdf</td>
+            </tr>
+          </table>
+        </div>
+      </td>
+    </tr>
+    </Fragment>); 
 }
 
 const EditableRow = ({ editForm, handleEditChange, handleCancel }) => {
@@ -75,7 +104,14 @@ const TableCrud = ({ filter }) => {
     name : "",
     email: "",
     rol: "",
-  })
+  });
+
+
+  const [addFormData, setAddFormData] = useState({
+    name : "",
+    email: "",
+    rol: "",
+  });
 
   const handleEditClick = (event, user) => {
     event.preventDefault(); //preventDefault() cancela el evento si es cancelable, lo que significa que la acción predeterminada que pertenece al evento no ocurrirá.
@@ -136,62 +172,94 @@ const TableCrud = ({ filter }) => {
     setUsers(newUsers);
   }
 
+  const handleDesplegarFormulario = () => {
+    document.getElementById("addUserForm").style.display = "block";
+    document.getElementById("btnAddUser").style.display = "none";
+  }
+  
+  const handleOcultarFormShowBtn = () => {
+    document.getElementById("addUserForm").style.display = "none";
+    document.getElementById("btnAddUser").style.display = "block";
+  }
+
+  //Habrá que hacer un cambio de estado de visible a invisible
+  const handleDisplayTable = (userId) => {
+
+    var elemento = document.getElementById(`row-suplementary${userId}`);
+    console.log(elemento);
+    elemento.style.display = "table-row"
+  }
+
   let attrFilter = '';
   if(filter == false) {
     attrFilter = 'hidde';
   }
   
-  return (<form onSubmit={handleSaveFormSubmit}>
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Role</th>
-          <th>Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className={attrFilter}> 
-          <td><input className="form-control"></input></td>
-          <td><input className="form-control"></input></td>
-          <td><input className="form-control"></input></td>
-          <td></td>
-        </tr>
-        {
-          users.map((user) => (
-            <Fragment key={user.id}>
-              {
-                editUserId === user.id ? <EditableRow editForm={editFormData} handleCancel={handleCancelClick} handleEditChange={handleEditFormChange}/> : <ReadOnlyRow element={user} handleEdit={handleEditClick} handleDelete={handleDeleteClick}/>
-              }
-            </Fragment>
-          ))
-        }
-      </tbody>
-    </table>
-    </form>);   
+  return (<Fragment>
+        <div className="row">
+            <div className="col-12 newUserCol">
+              <AddUser desplegarFormulario={handleDesplegarFormulario} ocultarFormShowBtn={handleOcultarFormShowBtn}/>
+            </div>
+          </div>
+          <div className="row">
+              <div className="col-12">
+                <form onSubmit={handleSaveFormSubmit}>
+                <table className="table">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Role</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className={attrFilter}> 
+                      <td><input className="form-control"></input></td>
+                      <td><input className="form-control"></input></td>
+                      <td><input className="form-control"></input></td>
+                      <td></td>
+                    </tr>
+                    {
+                      users.map((user) => (
+                        <Fragment key={user.id}>
+                          {
+                            editUserId === user.id ? <EditableRow editForm={editFormData} handleCancel={handleCancelClick} handleEditChange={handleEditFormChange}/> : <ReadOnlyRow element={user} handleEdit={handleEditClick} handleDelete={handleDeleteClick} handleDisplay={handleDisplayTable}/>
+                          }
+                        </Fragment>
+                      ))
+                    }
+                  </tbody>
+                </table>
+                </form>
+              </div>
+          </div>
+        </Fragment>
+  );   
 }; 
 
-//Esto habrá que cambiarlo, era solo para hacer idea de como colocarlo
-const AddUser = () => {
+const AddUser = ({desplegarFormulario, ocultarFormShowBtn}) => {
   return (<Fragment>
-            <button className="btn btn-primary">Añadir nuevo usuario</button>
-          </Fragment>);
+        <button className="btn btn-primary" id="btnAddUser" type="button" onClick={desplegarFormulario}>Añadir nuevo usuario</button>
+        <form id="addUserForm">
+            <h4>Formulario de registro de usuarios</h4>
+            <label>Nombre</label><input className="form-control" placeholder="Nombre" name="name" required="required"></input>
+            <label>Email</label><input className="form-control" placeholder="Email" name="email" required="required"></input>
+            <label>Rol</label><select className="form-select" name="" required="required">
+              <option>Admin</option>
+              <option>User</option>
+            </select>
+            <button className="btn btn-primary" type="submit">Guardar</button>
+            <button className="btn btn-danger" type="button" onClick={ocultarFormShowBtn}>Cancelar</button>
+        </form>
+    </Fragment>);
 }
+
 
 function App() {
   return (
     <div className="container">
-        <div className="row">
-            <div className="col-12 newUserCol">
-                <AddUser/>
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-12">
-              <TableCrud filter={false}/>
-            </div>
-        </div>
+        <TableCrud filter={false}/>
     </div>
   );
 }
