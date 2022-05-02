882e5c47d2fb6f5e53d8cf5390aa41df3a67403f
Nuria <nuria.villaronga@xercode.com>
2022-05-02 13:25:05 +0200

    password update (OK) --> acabar confirm_password

--- src/Components/Content/AdministrationContent/PasswordForm/InputPassword.jsx
+++ src/Components/Content/AdministrationContent/PasswordForm/InputPassword.jsx
@@ -7,15 +7,17 @@ function InputPassword ({type ="text", placeholder, name, required = "", status,
     var style_error ="";
   
     const validation = (event) => { 
-      if (regexp) {
+        if (regexp) {
             if(regexp.test(event.target.value)) { 
               changeStatus({...status, field: event.target.value, valid: "validado"}) 
             }
-            else {
-              changeStatus({...status, field: '-', valid: "no_validado"})
-              //Cuando field value sea = "" tendrá que ser válido (no es obligaotiro modificar las contraseña)
+            else if (event.target.value == "") {
+              changeStatus({...status, field: event.target.value, valid: "validado"})
             }
-          }
+            else {
+              changeStatus({...status, field: event.target.value, valid: "no_validado"})
+            }
+        }
     }
   
     if(status.valid == "no_validado") {
@@ -28,7 +30,7 @@ function InputPassword ({type ="text", placeholder, name, required = "", status,
     }
   
       return (<Fragment>
-                <input className={`form-control ${style_input}`} type={type} placeholder={placeholder} name={name} regexp={regexp} required={required} onKeyUp={validation} onClick={console.log(status)}/>
+                <input className={`form-control ${style_input}`} type={type} placeholder={placeholder} name={name} regexp={regexp} required={required} onKeyUp={validation} onClick={console.log(status.field)}/>
                 <div className={style_error}>{error_message}</div>
             </Fragment>
       );
